a6b3842d9d746c427e537979826da24a279db593
ErikKalkoken <erik.kalkoken@gmail.com>
2024-08-29 15:58:47 +0200

    Restructure cli command and add dump feature

--- package_monitor/tests/test_commands.py
+++ package_monitor/tests/test_commands.py
@@ -8,11 +8,19 @@ from app_utils.testing import NoSocketsTestCase
 PACKAGE_PATH = "package_monitor.management.commands"
 
 
-@patch(PACKAGE_PATH + ".package_monitor_refresh.Distribution.objects.update_all")
-class TestPurgeAll(NoSocketsTestCase):
+@patch(PACKAGE_PATH + ".packagemonitorcli.Distribution.objects.update_all")
+class TestRefresh(NoSocketsTestCase):
     def test_can_purge_all_data(self, mock_update_all):
         mock_update_all.return_value = 0
 
         out = StringIO()
-        call_command("package_monitor_refresh", stdout=out)
+        call_command("packagemonitorcli", "refresh", stdout=out)
         self.assertTrue(mock_update_all.called)
+
+
+class TestDump(NoSocketsTestCase):
+    def test_can_dump_packages(self):
+        out = StringIO()
+        call_command("packagemonitorcli", "dump", stdout=out)
+        o = out.getvalue()
+        self.assertIn("pip", o)

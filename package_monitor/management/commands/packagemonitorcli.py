a333f850f198f8e0addbf05ad8c31c04d1974543
ErikKalkoken <erik.kalkoken@gmail.com>
2024-08-29 16:36:56 +0200

    Filter out some fields when dumping by default

--- package_monitor/management/commands/packagemonitorcli.py
+++ package_monitor/management/commands/packagemonitorcli.py
@@ -16,6 +16,9 @@ except ImportError:
 
 COMMAND = "command"
 
+EXCLUDED_FIELDS = {"description", "description_content_type", "license"}
+"""Fields excluded by default in the dump command. But can be included with the --all flag."""
+
 
 class Command(BaseCommand):
     help = "Package monitor CLI tool"
@@ -38,34 +41,46 @@ class Command(BaseCommand):
             choices=["json", "yaml"],
             help="Data format",
         )
+        dump.add_argument(
+            "-a",
+            "--all",
+            action="store_true",
+            help=f"Include these normally omitted metadata fields: {EXCLUDED_FIELDS}",
+        )
         subparsers.add_parser("refresh", help="Refresh distribution packages")
 
     def handle(self, *args, **options):
         command = options[COMMAND]
         if command == "dump":
-            self.dump(options["format"])
+            self.dump(options["format"], options["all"])
         elif command == "refresh":
             self.refresh()
         else:
             raise NotImplementedError(command)
 
-    def dump(self, format: str):
+    def dump(self, format: str, show_all: bool):
         distributions = {}
         for i, d in enumerate(importlib_metadata.distributions(), start=1):
             files = [str(f) for f in d.files]
+            metadata = d.metadata.json
+            if not show_all and isinstance(metadata, dict):
+                metadata = {
+                    k: v for k, v in metadata.items() if k not in EXCLUDED_FIELDS
+                }
             x = {
                 "name": d.name,
                 "normalized_name": d._normalized_name,
                 "version": d.version,
                 "requires": d.requires,
                 "files": files,
-                "metadata": d.metadata.json,
+                "metadata": metadata,
             }
             k = d._normalized_name
             while True:
                 if k not in distributions:
                     break
                 k += "_"
+                x["duplicate"] = True
             distributions[k] = x
 
         if format == "json":
